d491ff57cc8a52489b542abdf95ef442712a52e5
soroushshalali <soroushshalali@gmail.com>
2020-04-14 15:52:35 +0200

    v0.0

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        Test
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

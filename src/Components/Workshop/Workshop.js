588dce3c51c800a68e14a6cc1367c47f9275eb52
Edward Agyeman <edw.agyeman@gmail.com>
2020-04-15 12:27:32 +0200

    workshop

---
+++ src/Components/Workshop/Workshop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Workshop = () => {
+    return (
+        <>
+            <section className="lerne-coden">
+            Von Anfänger bis Business –
+            lerne Coden in einem unserer Workshops.
+            </section>
+            <section></section>
+        </>
+     );
+}
+ 
+export default Workshop;

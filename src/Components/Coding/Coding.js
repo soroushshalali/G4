70fae78ad1c31d9bb5142f61a744f35fd6990ed9
soroushshalali <soroushshalali@gmail.com>
2020-04-15 11:07:33 +0200

    img

--- src/Components/Coding/Coding.js
+++ src/Components/Coding/Coding.js
@@ -4,7 +4,15 @@ import './codingStyle.css'
 const Coding = () => {
     return (
         <header className="coding" >
-            <span><i class="fas fa-chevron-down"></i></span>
+            <section className="img-sec" >faslhfakjf</section>
+            <section className="title-sec" >
+                <div>
+                    <span><i class="fas fa-chevron-down"></i></span><br></br>
+                    <span><i class="fas fa-chevron-down"></i></span>
+                </div>
+                <h1>Teste deine<br></br>Codingskills<br></br>in einem<br></br>unserer<br></br><span>Workshops</span></h1>
+                <h5><i class="fas fa-phone-alt"></i>2384273864872364</h5>
+            </section>
         </header>
     );
 }

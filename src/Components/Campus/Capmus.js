dd0d64e078c7395bc8c23ce368f6dae617d5c130
Florian Bollmann <florian@gentlent.com>
2020-04-15 11:43:45 +0200

    Florian

---
+++ src/Components/Campus/Capmus.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './campusStyle.css'
+
+const Campus = () => {
+  return (
+    <div className="campussection">
+      <div className="campusTitle">
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Campus;

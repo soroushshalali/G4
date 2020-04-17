90efb6c7678af53e2788f771d288d8ff2b93ce91
odify <odify495@gmail.com>
2020-04-17 15:19:38 +0200

    xyz3

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
 import Workshop from './Components/Workshop/Workshop.js';
-import Campus from './Components/Campus/Capmus.js';
-import Coding from './Components/Coding/Coding.js';
-import Form from './Components/Form/Form.js';
+
+import Form from './Components/Form/Form';
+import Coding from './Components/Coding/Coding';
+import Programming from './Components/Coding/Programming';
+import Campus from './Components/Campus/Capmus';
+import Home from './Components/Home';
 
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
-        <Workshop />
-        <Form />
+        <Route path='/' component={Home} exact />
+        <Route path='/contact' component={Form} />
       </div>
     </BrowserRouter>
   );

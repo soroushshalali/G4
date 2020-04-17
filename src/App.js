362415d4901174e8ec515a4a63d3937538445542
odify <odify495@gmail.com>
2020-04-17 09:04:42 +0200

    v.02

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route } from "react-router-dom";
+import Coding from './Components/Coding/Coding';
+import Workshop from './Components/Workshop/Workshop';
+import Campus from './Components/Campus/Capmus';
 import Form from './Components/Form/Form';
+import Icon from './Icon';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        {/* <Form /> */}
+        <Icon />
+        <Route path='/Components/Coding' component={Coding} exact />
       </div>
     </BrowserRouter>
   );

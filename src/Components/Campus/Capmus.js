17e115ef01fcc6eb195bff682a9edb9c4cffe93d
Florian Bollmann <florian@gentlent.com>
2020-04-20 14:57:59 +0200

    campus

--- src/Components/Campus/Capmus.js
+++ src/Components/Campus/Capmus.js
@@ -3,53 +3,29 @@ import './campusstyle.css';
 
 const Campus = () => {
   return (
-    <div className="campussection">
-      <div className="title-image">
-        <img src="img/_DSC2464_LowRes.jpg" alt="Bild" className="image" />
-      </div>
-      <div className="campusTitle">
-        <h1 className="title">Unser Campus ist <span className="inverted">mehr</span></h1>
-      </div>
-      <div className="article-whereAreWe">
-        <p className="paragrapf">
-          Unsere Schule befindet sich im Super7000. Auf über 1000 qm arbeiten mehr als 50 Unternehmen in offenen Büros und in Teambüros inklusive Telefonzellen, Meetingräumen, Bällebad und der legendären Eventfläche auf echtem Kunstrasen für Workshops, Meet-Ups Co. Wir nutzen dieses einzigartige Umfeld, um Dich noch schneller in passende Jobs zu vermitteln.
-        </p>
-        <div className="hashtag">
-          <p className="itemImage">
-            #superwinwin
-          </p>
-        </div>
-      </div>
-      <div>
-        <img src="public/img/_DSC2513_LowRes.jpg" alt="gemeinsam Spaß am coden" />
-      </div>
-      <div>
-        <img src="public/img/...."></img>
-      </div>
-      <div className="article-two">
-        <h2>
-          <span className="char1">S</span>
-          <span className="char2">U</span>
-          <span className="char3">P</span>
-          <span className="char4">E</span>
-          <span className="char5">R</span>
-          <span className="char6">(</span>
-          <span className="char7">7</span>
-          <span className="char8">0</span>
-          <span className="char9">0</span>
-          <span className="char10">0</span>
-          <span className="char11">)</span>
-        </h2>
-        <p className="paragrapf">
-          SuperCode bietet ein arbeitsmarktlich aussichtsreich und praxisnahes Bildungsangebot im IT-Bereich mit methodisch modernen und hohem Einsatz von Lehrpersonal. Hervorzuheben ist die hervorragende Branchenvernetzung sowie die innovative Praxiskompetenz der Geschäftsführung und des Teams.
-        </p>
-        <div className="hashtag">
-          <p>
-            # Auszug aus dem Certqua Auditbericht
-          </p>
-        </div>
-      </div>
-    </div>
+<div className="grid">
+  <img src="img/_DSC2513_LowRes.jpg" alt="Codeing together" className="photo1" />
+  <h2 className="title">Unser Campus ist <span className="outline">mehr</span></h2>
+  <div className="article-1">
+    <p>Unsere Schule befindet sich im Super7000. Auf über 1.000qm arbeiten mehr als 50 Unternehmen in offenen
+      Büros und in Teambüros inklusive Telefonzellen, Meetingräumen, Bällebad und der legendären Eventfläche
+      auf echtem Kunstrasen für Workshops, Meet-Ups & Co. Wir nutzen dieses einzigartige Umfeld, um Dich noch
+      schneller in passende Jobs zu vermitteln.</p>
+    <p className="hashtag">#superwinwin</p>
+  </div>
+  <img src="img/Super-Code__DSC2417_LowRes.jpg" alt="" className="photo2" />
+  <img src="img/_DSC2765_LowRes.jpg" alt="" className="photo3" />
+  <img src="img/jason-coudriet-dc80wb2nRHk-unsplash.jpg" alt="" className="photo4" />
+  <img src="img/super7000_kreis.png" alt="Super7000 Kreis" className="superkreis" />
+  <img src="img/_DSC2464_LowRes.jpg" alt="" className="photo5" />
+  <img src="img/_Super-Code__DSC2412_Campus.jpg" alt="" className="photo6" />
+  <div className="article-2">
+    <p>SuperCode bietet ein arbeitsmarktlich aussichtsreiches und praxisnahes Bildungsangebot im IT-Bereich mit
+      methodisch modernen und hohem Einsatz von Lehrpersonal. Hervorzuheben ist die hervorragende
+      Branchenvernetzung sowie die innovative Praxiskompetenz der Geschäftsführung und des Teams</p>
+    <p className="hashtag"># Auszug aus dem Certqua Auditbericht</p>
+  </div>
+</div>
   );
 }
 

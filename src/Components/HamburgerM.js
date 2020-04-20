37e204d530901fd0814392ecdcaa7dcf348d271f
odify <odify495@gmail.com>
2020-04-20 17:28:44 +0200

    xyz7

--- src/Components/HamburgerM.js
+++ src/Components/HamburgerM.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './hmStyle.css';
 import { Link } from 'react-router-dom'
+import FadeIn from 'react-fade-in';
 
 
 const HamburgerM = () => {
@@ -9,20 +10,23 @@ const HamburgerM = () => {
     const [className1, setClassName1] = useState('');
     const [className2, setClassName2] = useState('');
     const [className3, setClassName3] = useState('');
+
     function changeLink() {
         setTest(!test)
         test ? setLink('/') : setLink('/contact');
         test ? setClassName1('span1') : setClassName1('');
         test ? setClassName2('span2') : setClassName2('');
         test ? setClassName3('span3') : setClassName3('');
+
     }
 
-    return (
+    return (<FadeIn>
         <Link className='linkHM' onClick={changeLink} to={link} >
-            <span className={className1} >--------</span>
-            <span className={className2} >--------</span>
-            <span className={className3} >--------</span>
-        </Link>
+            <span className={className1} >----------</span>
+            <span className={className2} >----------</span>
+            <span className={className3} >----------</span>
+
+        </Link></FadeIn>
     );
 }
 
